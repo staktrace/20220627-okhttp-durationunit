d265a0dfc4ef2bf10f03870c2c2f40321e61595e
Kartikaya Gupta <kartikaya@squareup.com>
2022-06-28 13:21:09 -0400

    Cleanup

--- consumer/build.gradle.kts
+++ consumer/build.gradle.kts
@@ -1,17 +1,12 @@
 buildscript {
     repositories {
-        mavenLocal()
         mavenCentral()
     }
 
     dependencies {
         classpath(files("../plugin/build/libs/plugin.jar"))
-        classpath("com.squareup.okhttp3:okhttp:5.0.0-SNAPSHOT")
+        classpath("com.squareup.okhttp3:okhttp:5.0.0-alpha.10") // this isn't needed if you publish plugin.jar and use the pom file to resolve dependencies, but we're relying on the raw jar so we need this
     }
-}
-
-plugins {
-    // kotlin("jvm") version "1.6.21"
 }
 
 apply<MyPlugin>()
--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -1,14 +1,11 @@
 plugins {
     `kotlin-dsl`
-    // kotlin("jvm") version "1.6.21"
 }
 
 repositories {
-    mavenLocal()
     mavenCentral()
 }
 
 dependencies {
-    implementation("com.squareup.okhttp3:okhttp:5.0.0-SNAPSHOT")
-    // implementation("com.squareup.okhttp3:okhttp:5.0.0-alpha.10")
+    implementation("com.squareup.okhttp3:okhttp:5.0.0-alpha.10")
 }
